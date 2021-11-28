6b8d899ec6a60ac8e62433c8f5dd4973e8b6880a
Rushil Goel <rushil.goel2019@vitstudent.ac.in>
2021-11-28 20:34:42 +0530

    db data hashed. Security added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, jsonify
 import joblib
 import random
 import string
+import hashlib
 
 app = Flask(__name__)
 
@@ -19,7 +20,7 @@ def password_string_generator(length):
     result = ''.join(random.sample(result, len(result)))
     return result
 
-vectorizer = joblib.load("vectorizer1.pkl")
+vectorizer = joblib.load("count_vectorizer.pkl")
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -31,7 +32,7 @@ def predict():
     arr = []
     arr.append(pwd)
     vec_predict = vectorizer.transform(arr)
-    model = joblib.load("decisionTreeModel.pkl")
+    model = joblib.load("svm_model_all_data.pkl")
     prediction = model.predict(vec_predict)
     print("Strength is " + prediction)
     return render_template("result.html", prediction = password_hashmap[prediction[0]])
@@ -41,27 +42,33 @@ def generate():
     prediction = [-1]
     arr = []
     while int(prediction[0]) != 2:
-        generated_password = password_string_generator(3)
+        generated_password = password_string_generator(6)
+        print("Trying... " + generated_password)
         with open('generated_passwords.txt') as f:
             passwords = f.readlines()
-            if generated_password in passwords:
+            hash_pass = hashlib.sha256(generated_password.encode())
+            if hash_pass.hexdigest() in passwords:
                 redirect("/generate")
                 break
             else:
                 arr.append(generated_password)
                 vec_predict = vectorizer.transform(arr)
-                model = joblib.load("decisionTreeModel.pkl")
+                model = joblib.load("svm_model_all_data.pkl")
                 prediction = model.predict(vec_predict)
     print(generated_password, password_hashmap[prediction[0]])
     with open('generated_passwords.txt', 'a') as f:
+        f.write(hash_pass.hexdigest() + "\n")
         f.write(generated_password + "\n")
     return redirect("/show_generated")
 
 @app.route("/show_generated", methods=["GET"])
 def view_generated():
-    with open('generated_passwords.txt') as f:
+    with open('generated_passwords.txt', 'r+') as f:
         passwords = f.readlines()
-    generated_password = passwords[-1]
+        generated_password = passwords[-1]
+        f.seek(0)
+        f.truncate()
+        f.writelines(passwords[:-1])
     return render_template("generate.html", generated_password = generated_password)
 
 @app.route("/api/<string:password>", methods=['GET'])
@@ -70,7 +77,7 @@ def api_get(password):
     arr = []
     arr.append(pwd)
     vec_predict = vectorizer.transform(arr)
-    model = joblib.load("decisionTreeModel.pkl")
+    model = joblib.load("svm_model_all_data.pkl")
     prediction = model.predict(vec_predict)
     print("Strength is " + prediction)
     return jsonify({"Strength": password_hashmap[prediction[0]]})
@@ -83,7 +90,7 @@ def api_post():
         arr = []
         arr.append(pwd)
         vec_predict = vectorizer.transform(arr)
-        model = joblib.load("decisionTreeModel.pkl")
+        model = joblib.load("svm_model_all_data.pkl")
         prediction = model.predict(vec_predict)
         print("Strength is " + prediction)
         return jsonify({"Strength": password_hashmap[prediction[0]]})
